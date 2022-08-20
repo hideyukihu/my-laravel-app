0535791cfb80732121c3e18631d5c210b9ef7117
hide <majihaik@gmail.com>
2022-08-20 10:48:49 +0900

    bladeで値の埋め込み

--- app/resources/views/index.blade.php
+++ app/resources/views/index.blade.php
@@ -1,3 +1,10 @@
+<?php
+
+var_dump($posts);
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -5,10 +12,17 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
   <h1>MyBBS</h1>
+  <ul>
+    {{-- <li><?php echo e($posts[0])?></li> --}}
+    <li>{{ $posts[0] }}</li>
+    <li>title</li>
+    <li>title</li>
+  </ul>
 
-  
+
 </body>
 </html>
