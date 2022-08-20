c47305b7058fb642c759927a481d6051fced9e02
hide <majihaik@gmail.com>
2022-08-20 16:18:48 +0900

    詳細ページ実装　

--- app/app/Http/Controllers/PostController.php
+++ app/app/Http/Controllers/PostController.php
@@ -15,8 +15,11 @@ class PostController extends Controller
 
     public function index()
     {
-
         return view('index')->with(['posts' => $this->posts]);
+    }
+
+    public function show($id) {
+        return view('posts.show')->with(['post' => $this->posts[$id]]);
     }
 
 }
