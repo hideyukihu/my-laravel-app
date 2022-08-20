bb5dfec320170496d1a0215b095d313ec21956e9
hide <majihaik@gmail.com>
2022-08-20 17:30:40 +0900

    slot

--- app/resources/views/index.blade.php
+++ app/resources/views/index.blade.php
@@ -1,23 +1,8 @@
-<?php
-
-
-
-?>
-
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{url('css/style.css')}}">
-</head>
-
-<body>
+<x-layout>
+    <x-slot name='title'>
+        MY BBS
+    </x-slot>
     <h1>MyBBS</h1>
-
 
     <ul>
         @forelse ($posts as $index => $post)
@@ -30,8 +15,4 @@
             <li>No Pot yet</li>
         @endforelse
     </ul>
-
-
-</body>
-
-</html>
+</x-layout>
--- app/resources/views/posts/show.blade.php
+++ app/resources/views/posts/show.blade.php
@@ -1,27 +1,11 @@
-<?php
+<x-layout>
+    <x-slot name='title'>
+       {{$post}}- MY BBS
+    </x-slot>
 
-
-
-?>
-
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{url('css/style.css')}}">
-</head>
-
-<body>
     <div class="back-link">
         &laquo; <a href="{{route('posts.index') }}">戻る</a>
     </div>
     <h1>{{$post}}</h1>
 
-
-</body>
-
-</html>
+</x-layout>
