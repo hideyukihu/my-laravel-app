b1fd0b131b6bc6c2678ff03d9f4573a9445c3112
hide <majihaik@gmail.com>
2022-08-26 17:16:31 +0900

    投稿フォームの作成

--- example-app/resources/views/tweet/index.blade.php
+++ example-app/resources/views/tweet/index.blade.php
@@ -9,6 +9,16 @@
 <body>
     <h1>つぶやくよ</h1>
     <div>
+        <p>投稿フォーム</p>
+        <form action="{{ route('tweet.create') }}" method="POST">
+            @csrf
+            <label for="tweet-content">つぶやき</label>
+            <span>140字まで</span>
+            <textarea name="tweet" id="tweet-content" cols="30" rows="10" type="text" placeholder="つぶやきを入力"></textarea>
+            <button type="submit">投稿</button>
+        </form>
+    </div>
+    <div>
         @foreach ($tweets as $tweet)
         <p>{{$tweet->content}}</p>
         @endforeach
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -21,7 +21,10 @@ Route::get('/sample', [\App\Http\Controllers\sample\IndexController::class, 'sho
 
 Route::get('/sample/{id}', [\App\Http\Controllers\sample\IndexController::class, 'showId']);
 
-Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class);
+Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)->name('tweet.index');
+
+Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)->name('tweet.create');
+
 
 
 
