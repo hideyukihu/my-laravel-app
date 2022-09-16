5385e63ecf008433f5da4a457abb4d5085abd322
hide <majihaik@gmail.com>
2022-09-17 01:02:51 +0900

    32 サービスコンテナ

--- test/routes/web.php
+++ test/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComponentTestController;
+use App\Http\Controllers\LifeCycleTestController;
+
+
+
 
 
 /*
@@ -25,6 +29,8 @@ Route::get('/dashboard', function () {
 
 Route::get('component-test1', [ComponentTestController::class, 'showComponent1']);
 Route::get('component-test2', [ComponentTestController::class, 'showComponent2']);
+Route::get('/servicecontainer-test', [LifeCycleTestController::class, 'showServiceContainerTest']);
+
 
 
 
