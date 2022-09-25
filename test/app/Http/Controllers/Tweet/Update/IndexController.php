5d2d484936a0aae481c918fe4b4a686bec620b92
hide <majihaik@gmail.com>
2022-09-25 09:02:51 +0900

    編集投稿画面の作成

--- test/app/Http/Controllers/Tweet/Update/IndexController.php
+++ test/app/Http/Controllers/Tweet/Update/IndexController.php
@@ -20,11 +20,12 @@ class IndexController extends Controller
     {
         //
         $tweetId = (int) $request->route('tweetId');
-        $tweet = Tweet::where('id', $tweetId)->first();
+        $tweet = Tweet::where('id', $tweetId)->firstOrFail();
         if(is_null($tweet)) {
             throw new NotFoundHttpException('存在しないつぶやきです');
         }
 
-        dd($tweet);
+        return view('tweet.update')->with('tweet', $tweet);
+
     }
 }
