30621be0957bed3503ed3c10d68607d86d8f320e
hide <majihaik@gmail.com>
2022-10-08 19:00:27 +0900

    メール送信機能作成　途中

---
+++ test/app/Console/Commands/SendDailyTweetCountMail.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\DailyTweetCount;
+use App\Models\User;
+use App\Services\TweetService;
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Mail\Mailer;
+
+class SendDailyTweetCountMail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'mail:send-daily-tweet-count-mail';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '前日のつぶやき数を集計してつぶやきを促すメールを送ります';
+
+    public TweetService $tweetService;
+    public Mailer $mailer;
+
+    public function __construct(TweetService $tweetService, Mailer $mailer) {
+        parent::__construct();
+        $this->tweetService = $tweetService;
+        $this->mailer = $mailer;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        return 0;
+    }
+}
