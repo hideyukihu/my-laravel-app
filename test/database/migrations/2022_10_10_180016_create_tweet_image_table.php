0ef8c3a135d62cdd5ff2de05592efef40fc1b0c3
hide <majihaik@gmail.com>
2022-10-10 18:18:07 +0900

    画像用Eloquentモデルを作成する

---
+++ test/database/migrations/2022_10_10_180016_create_tweet_image_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tweet_image', function (Blueprint $table) {
+            $table->foreignId('tweet_id')->constrained('tweets')
+                ->cascadeOnDelete();
+            $table->foreignId('image_id')->constrained('image')
+                ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tweet_image');
+    }
+};
